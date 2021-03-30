b7f7d52571778ba0633b523e3e3040499f104900
urbanizzz <yuraoskin@gmail.com>
2021-03-30 12:52:37 +0700

    work on main file, checking of command line args

--- src/Main.hs
+++ src/Main.hs
@@ -1,5 +1,26 @@
 module Main where
 
+import System.Environment (getArgs)
+import Control.Monad (when,guard)
+
+errPrint :: String -> IO ()
+errPrint err = putStrLn $ err ++ " Use\nUBot {vk,tg} <token>"
+
+checkBotType :: String -> Bool
+checkBotType "vk" = True
+checkBotType "tg" = True
+checkBotType _ = False
+
 main :: IO ()
 main = do
-  putStrLn "hello world"
+  args <- getArgs
+  when (length args == 0) $ errPrint $ "Bot type is not specified."
+  guard (length args > 0)
+  let (botType:rest) = args
+  when (not . checkBotType $ botType) $ errPrint "Bot type is wrong." 
+  guard (checkBotType botType)
+  when (length rest == 0) $ errPrint $ "Bot token is not specified."
+  guard (length rest > 0)
+  let botToken = head rest
+  putStrLn $ "launch: bot " ++ botType ++ " " ++ botToken
+
