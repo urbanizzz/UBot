75bf3a1775f53bb182cc27a3948daf0777743046
urbanizzz <yuraoskin@gmail.com>
2021-05-13 09:59:39 +0700

    Bot refactoring
    - Bot: execHelpCommand and repeatMessage refactored to StateT monad
    - Bot: getBotEvent, execHelpCommand, execRepeatCommand and repeatMessage moved to the end of code

--- src/Bot.hs
+++ src/Bot.hs
@@ -34,19 +34,28 @@ createBot botType botToken = do
   (a,s) <- (runStateT (mainBot $ newHandle botType)) $ newState botType botToken
   return a
 
-getBotEvent :: Handle -> IO Event
-getBotEvent handle = getEvent handle
-
 helpMessage :: Environment -> Value
 helpMessage = stringToValue . about . config
 
-execHelpCommand :: Handle -> EventEscort -> Environment -> IO ()
-execHelpCommand handle escort env = do
-  let helpMsg = helpMessage $ env
-  (sendHelp handle) escort helpMsg
+getUserName :: EventEscort -> UserName
+getUserName = userName
+
+getUserRepeat :: UserName -> Environment -> RepeatNumber
+getUserRepeat name st = case M.lookup name (unUsersRepeat . usersRepeat $ st) of
+  Just rep -> rep
+  Nothing -> RepeatNumber . repeatDefault . config $ st
 
 repeatQuestionText :: Environment -> Value
 repeatQuestionText = stringToValue . repeatQuestion . config
+
+getBotEvent :: Handle -> IO Event
+getBotEvent handle = getEvent handle
+
+execHelpCommand :: Handle -> EventEscort -> StateT Environment IO ()
+execHelpCommand handle escort = do
+  st <- get
+  let helpMsg = helpMessage $ st
+  lift $ sendHelp handle escort helpMsg
 
 execRepeatCommand :: Handle -> EventEscort -> StateT Environment IO ()
 execRepeatCommand handle escort = do
@@ -60,26 +69,19 @@ execRepeatCommand handle escort = do
   let stNew = st {usersRepeat = UsersRepeat newRepeatMap}
   put stNew
 
-repeatMessage :: Handle -> EventEscort -> Environment -> IO ()
-repeatMessage handle escort st = replicateM_ num act
-  where num = unRepeatNumber . getUserRepeat name $ st
-        name = getUserName escort
-        act = sendMessage handle escort
-
-getUserName :: EventEscort -> UserName
-getUserName = userName
-
-getUserRepeat :: UserName -> Environment -> RepeatNumber
-getUserRepeat name st = case M.lookup name (unUsersRepeat . usersRepeat $ st) of
-  Just rep -> rep
-  Nothing -> RepeatNumber . repeatDefault . config $ st
+repeatMessage :: Handle -> EventEscort -> StateT Environment IO ()
+repeatMessage handle escort = do
+  st <- get
+  let name = getUserName escort
+  let num = unRepeatNumber . getUserRepeat name $ st
+  lift $ replicateM_ num $ sendMessage handle escort
 
 mainBot :: Handle -> StateT Environment IO ()
 mainBot handle = forever $ do
   st <- get
   event <- lift $ getBotEvent handle
   case event of
-    HelpCommand escort    -> lift $ execHelpCommand handle escort st
+    HelpCommand escort    -> execHelpCommand handle escort
     RepeatCommand escort  -> execRepeatCommand handle escort
-    Message escort        -> lift $ repeatMessage handle escort st
+    Message escort        -> repeatMessage handle escort
 
